807c83facfdc1a2f02d3c3774ac163706030f780
Alexandre Marinho <lyralemos@gmail.com>
2017-06-26 14:53:50 -0300

    Initial code commit

---
+++ project/__init__.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# instanciate the db
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db.init_app(app)
+
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
